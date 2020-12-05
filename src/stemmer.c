ceac8210ecd4e804ab6ea0cf4f333518d9ae1252
yoonjin09 <63450174+yoonjin09@users.noreply.github.com>
2020-12-06 06:56:08 +0900

    Update stemmer.c

--- src/stemmer.c
+++ src/stemmer.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "../include/libstemmer.h"
 
 int main()
@@ -10,13 +11,16 @@ int main()
 	char negativecsv[9090][200];
 	char tokenNegativeCsv[9090][200];
 	int countNegativeline = 0;
+	int endline =0;
+	char endl[2] = "\n";
+	char space[2] = " ";
 	if (fp == NULL)
 	{
-		printf("Wrong");
+		printf("Can not read train.negative.csv");
 	}
 	else
 	{
-		printf("it is ok\n");
+		printf("Read train.negative.csv\n");
 	}
 	//Read train.negative.csv
 
@@ -27,20 +31,18 @@ int main()
 
 	for (int index = 0; index < countNegativeline; index++)
 	{
-		char *ptr = strtok(negativecsv[index], " \"+/_-?!,.;$:()*");
+		char *ptr = strtok(negativecsv[index], " +_“”-?'.!,;$()*");
 		while (ptr != NULL)
 		{
-			char space[2] = " ";
 			strcat(tokenNegativeCsv[index], ptr);
 			strcat(tokenNegativeCsv[index], space);
-			ptr = strtok(NULL, " \"+/_-?!,.;$:()*");
+			ptr = strtok(NULL, " +_“”-?'.!,;$()*");
 		}
 	}
 	//Tokenization train.negative.csv and store tokenNegativeCsv array
 
 	//Start Normalization train.negative.csv
 	struct sb_stemmer *stemmer;
-	char buf[1024] = "connection ";
 
 	char afterStemNegative[9090][150];
 	stemmer = sb_stemmer_new("english", 0x0);
@@ -49,11 +51,19 @@ int main()
 		char *ptr = strtok(tokenNegativeCsv[index], " ");
 		while (ptr != NULL)
 		{
+			// if (strcmp(&ptr[strlen(ptr) - 1], "\n") == 0){
+			// 	ptr[strlen(ptr) - 1] = '\0';
+			// 	endline = 1;
+			// }
 			const char *s;
 			s = sb_stemmer_stem(stemmer, ptr, strlen(ptr));
-			char space[2] = " ";
+			
 			strcat(afterStemNegative[index], s);
 			strcat(afterStemNegative[index], space);
+			if(endline == 1){
+				strcat(ptr,"\n");
+			}
+			// endline=0;
 			ptr = strtok(NULL, " ");
 		}
 	}
@@ -61,14 +71,13 @@ int main()
 	//Finish Stemmer train.negative.csv
 
 	//Store afterstemNegative array to afterStemmerNegative.txt
-	FILE *fpAfterStemNe = fopen("afterStemmerNegative.txt", "a");
+	FILE *fpAfterStemNe = fopen("./alltext/afterStemmerNegative.txt", "a");
 	for (int index = 0; index < countNegativeline; index++)
 	{
 		fputs(afterStemNegative[index], fpAfterStemNe);
 	}
 	fclose(fpAfterStemNe);
 	fclose(fp);
-	printf("난 행복해\n");
 
 	//Start none-negative.csv tokenization
 	FILE *fpnone = fopen("../data/train.non-negative.csv", "r");
@@ -77,11 +86,11 @@ int main()
 	int countNonNegativeline = 0;
 	if (fpnone == NULL)
 	{
-		printf("Wrong");
+		printf("Can not read train.non-negative.csv");
 	}
 	else
 	{
-		printf("it is ok\n");
+		printf("Read train.non-negative.csv\n");
 	}
 	while (fgets(nonnegativecsv[countNonNegativeline], sizeof(nonnegativecsv[countNonNegativeline]), fpnone) != NULL)
 	{
@@ -90,14 +99,13 @@ int main()
 
 	for (int index = 0; index < countNonNegativeline; index++)
 	{
-		char *ptr = strtok(nonnegativecsv[index], " \"“”+/_-?!,.;$:()*");
+		char *ptr = strtok(nonnegativecsv[index], " “”.+'_-?!,;$()*");
 		while (ptr != NULL)
 		{
-			char space[2] = " ";
-			// printf("%s\n",ptr);
+			
 			strcat(tokenNonNegativeCsv[index], ptr);
 			strcat(tokenNonNegativeCsv[index], space);
-			ptr = strtok(NULL, " \"“”+/_-?!,.;$:()*");
+			ptr = strtok(NULL, " “”.+'_-?!,;$()*");
 		}
 	}
 	fclose(fpnone);
@@ -117,11 +125,15 @@ int main()
 		char *ptr = strtok(tokenNonNegativeCsv[index], " ");
 		while (ptr != NULL)
 		{
+			
 			const char *s;
 			s = sb_stemmer_stem(stemmer2, ptr, strlen(ptr));
-			char space[2] = " ";
 			strcat(nonnegativecsv[index], s);
 			strcat(nonnegativecsv[index], space);
+			if(endline == 1){
+				strcat(ptr,"\n");
+			}
+			// endline =0;
 			ptr = strtok(NULL, " ");
 		}
 	}
@@ -129,7 +141,7 @@ int main()
 	//Finish Stemmer train.negative.csv
 
 	//Store afterstemNegative array to afterStemmerNegative.txt
-	FILE *fpAfterStemNonNe = fopen("afterStemmerNonNegative.txt", "a");
+	FILE *fpAfterStemNonNe = fopen("./alltext/afterStemmerNonNegative.txt", "a");
 	for (int index = 0; index < countNonNegativeline; index++)
 	{
 		fputs(nonnegativecsv[index], fpAfterStemNonNe);
@@ -152,30 +164,32 @@ int main()
 	}
 	else
 	{
-		printf("Read allwords\n");
+		printf("Read allwords file\n");
 	}
 	while (fgets(allword[countword], sizeof(allword[countword]), fp) != NULL)
 	{
 		allword[countword][strlen(allword[countword]) - 1] = '\0';
 		countword++;
 	}
-	
+	fclose(fpAllwords);
 	/*
 	*	remove Stopword on negative.csv
 	*/
-	FILE *fpNegativeRemove = fopen("RemoveNegative.txt", "a");
-	printf("countNegativeline :%d\n", countNegativeline);
-
-	FILE * removeNegative = fopen("afterStemmerNegative.txt", "r");
-	char line[200] ;
-	while(fgets(line, sizeof(line), removeNegative) != NULL)
+	FILE *fpNegativeRemove = fopen("./alltext/RemoveNegative.txt", "a");
+	FILE *removeNegative = fopen("./alltext/afterStemmerNegative.txt", "r");
+	char line[200];
+	while (fgets(line, sizeof(line), removeNegative) != NULL)
 	{
 		char *ptr = strtok(line, " ");
-		char space[2] = " ";
 		int confirm = 0;
 		char temp[200] = "";
 		while (ptr != NULL)
-		{
+		{	
+			if (strcmp(&ptr[strlen(ptr) - 1], "\n") == 0){
+				ptr[strlen(ptr) - 1] = '\0';
+				endline = 1;
+			}
+				
 			for (int i = 0; i < countword; i++)
 			{
 				if (strcmp(ptr, allword[i]) == 0)
@@ -184,34 +198,53 @@ int main()
 					break;
 				}
 			}
+			if (strchr(ptr, '^') || strchr(ptr, '\"') || strchr(ptr, '@') || strchr(ptr, ':') || strchr(ptr, '/') || strchr(ptr, '#') || strchr(ptr, '\'') || strchr(ptr, '&') || strchr(ptr, '1') || strchr(ptr, '2') || strchr(ptr, '3') || strchr(ptr, '4') || strchr(ptr, '5') || strchr(ptr, '6') || strchr(ptr, '7') || strchr(ptr, '8') || strchr(ptr, '9') || strchr(ptr, '0') != NULL)
+			{
+				// printf("ptr :%s\n",ptr);
+				confirm = 1;
+			}
+			if (isupper(ptr[0]))
+			{
+				confirm = 1;
+			}
 			if (confirm == 0)
 			{
 				strcat(temp, ptr);
 				strcat(temp, space);
 			}
+			else
+			{
+				if (endline == 1)
+					strcat(temp, endl);
+			}
+			endline = 0;
 			confirm = 0;
 			ptr = strtok(NULL, " ");
 		}
 		fputs(temp, fpNegativeRemove);
 	}
-	
+	fclose(removeNegative);
+	fclose(fpNegativeRemove);
 	/*
 	*	remove Stopword on non-negative.csv
 	*/
-	FILE *fpNonNegativeRemove = fopen("RemoveNonNegative.txt", "a");
-	printf("countNegativeline :%d\n", countNonNegativeline);
+	FILE *fpNonNegativeRemove = fopen("./alltext/RemoveNonNegative.txt", "a");
+	FILE *removeNonNegative = fopen("./alltext/afterStemmerNonNegative.txt", "r");
+	char nonline[200];
+	int confirmcount = 0;
 
-	FILE * removeNonNegative = fopen("afterStemmerNonNegative.txt", "r");
-	char nonline[200] ;
-	while(fgets(nonline, sizeof(nonline), removeNonNegative) != NULL)
+	while (fgets(nonline, sizeof(nonline), removeNonNegative) != NULL)
 	{
-		
 		char *ptr = strtok(nonline, " ");
-		char space[2] = " ";
 		int confirm = 0;
 		char temp[200] = "";
+
 		while (ptr != NULL)
 		{
+			if (strcmp(&ptr[strlen(ptr) - 1], "\n") == 0){
+				ptr[strlen(ptr) - 1] = '\0';
+				endline = 1;
+			}
 			for (int i = 0; i < countword; i++)
 			{
 				if (strcmp(ptr, allword[i]) == 0)
@@ -220,21 +253,38 @@ int main()
 					break;
 				}
 			}
+			if (strchr(ptr, '^') || strchr(ptr, '\"') || strchr(ptr, '@') || strchr(ptr, ':') || strchr(ptr, '/') || strchr(ptr, '#') || strchr(ptr, '\'') || strchr(ptr, '&') || strchr(ptr, '1') || strchr(ptr, '2') || strchr(ptr, '3') || strchr(ptr, '4') || strchr(ptr, '5') || strchr(ptr, '6') || strchr(ptr, '7') || strchr(ptr, '8') || strchr(ptr, '9') || strchr(ptr, '0')  != NULL)
+			{
+				// printf("ptr :%s\n",ptr);
+				confirm = 1;
+				if (strcmp(&ptr[strlen(ptr) - 1], "\n") == 0)
+					strcat(temp, "\n");
+			}
+			if (isupper(ptr[0]))
+			{
+				confirm = 1;
+			}
 			if (confirm == 0)
 			{
 				strcat(temp, ptr);
 				strcat(temp, space);
 			}
+			else
+			{
+				if (endline == 1)
+					strcat(temp, "\n");
+			}
+			endline=0;
 			confirm = 0;
 			ptr = strtok(NULL, " ");
 		}
 		fputs(temp, fpNonNegativeRemove);
 	}
 
-	fclose(removeNegative);
 	fclose(removeNonNegative);
-	fclose(fpNegativeRemove);
+
 	fclose(fpNonNegativeRemove);
-	fclose(fpAllwords);
+
+	printf("FINISH\n");
 	return 0;
 }
